9fb28a1a28bcbf722f46f9853f2512aef5fab4f2
Dileesha Perera <dileeshaperera28@gmail.com>
2026-07-11 23:42:40 +0530

    Update genproto.sh

--- src/emailservice/genproto.sh
+++ src/emailservice/genproto.sh
@@ -16,6 +16,7 @@
 
 # [START gke_emailservice_genproto]
 
+
 python -m grpc_tools.protoc -I../../protos --python_out=. --grpc_python_out=. ../../protos/demo.proto
 
 # [END gke_emailservice_genproto]
